de5da4048c1c84a4f90ec8e19c24a377966c1b91
andrewdaunt <167640503+andrewdaunt@users.noreply.github.com>
2025-05-07 20:08:56 -0500

    Ui Changes
    - Added ui elements back
    - Added logic for exiting game when player reaches end

--- Assets/Scripts/KeyCollider.cs
+++ Assets/Scripts/KeyCollider.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyCollider : MonoBehaviour
 {
+    public Image keyImage;
     public AudioClip pickupSound;
+
+    void Start()
+    {
+        keyImage.enabled = false;
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            AudioSource.PlayClipAtPoint(pickupSound, transform.position, 10f); // 10x volume because it's so quiet
-
+            var playerScript = other.GetComponent<PlayerMovement>();
+            if(playerScript != null)
+            {
+                playerScript.hasKey = true;
+            }
+            AudioSource.PlayClipAtPoint(pickupSound, transform.position, 10f);
             Destroy(gameObject);
+            keyImage.enabled = true;
         }
     }
 }
